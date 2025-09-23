aedb4726634229ce55dcdc53c03a48072a5c1cae
jdettorreUC <dettorjt@mail.uc.edu>
2025-09-23 18:54:43 -0400

    enhancement start and more reverse tarots

--- main.lua
+++ main.lua
@@ -1,2 +1,10 @@
-assert(SMODS.load_file('jokers/page1.lua'))()
-assert(SMODS.load_file('consumables/reverse_tarots.lua'))()
+SMODS.current_mod.optional_features = {
+    cardareas = {
+        discard = true,
+        deck = true
+    }
+}
+
+assert(SMODS.load_file('content/enhancements.lua'))()
+assert(SMODS.load_file('content/jokers/page1.lua'))()
+assert(SMODS.load_file('content/consumables/reverse_tarots.lua'))()
